63a1884f681a3850034b4de781c6fe5b1ac0d6eb
JooYae <jooyae3970@naver.com>
2021-07-31 17:11:35 +0900

    [feat] splash, login navigation

--- app/src/main/java/org/journey/juyaemarshmallow/splash/OnBoardingActivity.kt
+++ app/src/main/java/org/journey/juyaemarshmallow/splash/OnBoardingActivity.kt
@@ -2,6 +2,7 @@ package org.journey.juyaemarshmallow.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.journey.juyaemarshmallow.R
 import org.journey.juyaemarshmallow.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -11,5 +12,8 @@ class OnBoardingActivity : AppCompatActivity() {
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.onboarding_container_view, OnboardingFragment())
+            .commitNow()
     }
 }
--- app/src/main/java/org/journey/juyaemarshmallow/splash/OnboardingFragment.kt
+++ app/src/main/java/org/journey/juyaemarshmallow/splash/OnboardingFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.coroutines.*
 import org.journey.juyaemarshmallow.R
 import org.journey.juyaemarshmallow.base.BaseFragment
 import org.journey.juyaemarshmallow.databinding.FragmentOnboardingBinding
+import kotlin.coroutines.CoroutineContext
 
-class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>() {
+class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,13 +22,25 @@ class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>() {
     ): View? {
         return inflater.inflate(R.layout.fragment_onboarding, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        launch{
+            delay(2000)
+            withContext(Dispatchers.Main){
+                Navigation.findNavController(binding.root)
+                    .navigate(R.id.action_onboardingFragment_to_loginFragment)
+            }
 
+        }
+    }
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentOnboardingBinding {
         return FragmentOnboardingBinding.inflate(inflater, container, false)
     }
+
+
 
 
 }
