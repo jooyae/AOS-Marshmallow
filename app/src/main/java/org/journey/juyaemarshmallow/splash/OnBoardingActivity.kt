cb5cf4cfd45d599880dd24a09cbf1094bb2e0c61
JooYae <jooyae3970@naver.com>
2021-08-01 17:20:20 +0900

    [feat] #10 NavController

--- app/src/main/java/org/journey/juyaemarshmallow/splash/OnBoardingActivity.kt
+++ app/src/main/java/org/journey/juyaemarshmallow/splash/OnBoardingActivity.kt
@@ -2,18 +2,23 @@ package org.journey.juyaemarshmallow.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import org.journey.juyaemarshmallow.R
 import org.journey.juyaemarshmallow.databinding.ActivityOnBoardingBinding
 
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavController()
+    }
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.onboarding_container_view, OnboardingFragment())
-            .commitNow()
+    private fun initNavController(){
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
