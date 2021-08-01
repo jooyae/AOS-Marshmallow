57c0b389b03ba8a2d3ae42449e787d7075142dd0
JooYae <jooyae3970@naver.com>
2021-08-01 18:06:24 +0900

    [feat] #13 DI Hwahae application

---
+++ app/src/main/java/org/journey/juyaemarshmallow/application/Hwahaeapplication.kt
@@ -0,0 +1,11 @@
+package org.journey.juyaemarshmallow.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class hwahaeapplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
