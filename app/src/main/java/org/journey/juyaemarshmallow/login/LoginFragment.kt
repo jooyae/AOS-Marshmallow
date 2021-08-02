887ac152a351d5dd366a93a8e1c26b332a087f1b
JooYae <jooyae3970@naver.com>
2021-08-02 16:05:06 +0900

    [feat] #19 Viewpager2 login

--- app/src/main/java/org/journey/juyaemarshmallow/login/LoginFragment.kt
+++ app/src/main/java/org/journey/juyaemarshmallow/login/LoginFragment.kt
@@ -1,16 +1,17 @@
 package org.journey.juyaemarshmallow.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import org.journey.juyaemarshmallow.R
 import org.journey.juyaemarshmallow.base.BaseFragment
 import org.journey.juyaemarshmallow.databinding.FragmentLoginBinding
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    private lateinit var viewPager: ViewPager2
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -20,6 +21,21 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadIndicator()
     }
+
+
+    private fun loadIndicator() {
+        val imageList = listOf (R.drawable.imagelist1, R.drawable.imagelist2)
+        val viewPager = binding.viewpagerLogin
+//        viewPager.setPageTransformer { page, position ->
+//            page.translationX = position
+//        }
+        viewPager.adapter = ViewPagerAdapter(imageList)
+        binding.indicatorLogin.setViewPager2(viewPager)
+        viewPager.offscreenPageLimit = 2
+    }
+
+
 
 }
