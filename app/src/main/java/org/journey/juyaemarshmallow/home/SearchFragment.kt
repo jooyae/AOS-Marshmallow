acbcb474c7c655b2eec00e7aa625a2edc2541c45
JooYae <jooyae3970@naver.com>
2021-08-03 17:07:48 +0900

    [feat] #21 Search Fragment

---
+++ app/src/main/java/org/journey/juyaemarshmallow/home/SearchFragment.kt
@@ -0,0 +1,21 @@
+package org.journey.juyaemarshmallow.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.journey.juyaemarshmallow.base.BaseFragment
+import org.journey.juyaemarshmallow.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSearchBinding {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/org/journey/juyaemarshmallow/home/ShopFragment.kt
@@ -0,0 +1,18 @@
+package org.journey.juyaemarshmallow.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.journey.juyaemarshmallow.base.BaseFragment
+import org.journey.juyaemarshmallow.databinding.FragmentShopBinding
+
+class ShopFragment : BaseFragment<FragmentShopBinding>() {
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentShopBinding {
+        TODO("Not yet implemented")
+    }
+}
