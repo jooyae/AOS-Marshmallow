8785648685119602278a02ed8675fbd529f86a52
JooYae <jooyae3970@naver.com>
2021-08-07 17:04:24 +0900

    [feat] #37 IndexViewStatus

---
+++ app/src/main/java/org/journey/juyaemarshmallow/IndexViewStatus.kt
@@ -0,0 +1,11 @@
+package org.journey.juyaemarshmallow
+
+object IndexViewStatus {
+    private val allSkinTypes = arrayOf("oily", "dry", "sensitive")
+    //default
+    var currentSkinType = allSkinTypes[0]
+    set(skinTypeIndex){
+        field = allSkinTypes[skinTypeIndex.toInt()]
+    }
+    var currentSearchKeyWord : String?= null
+}
